0a6cb43981da523824bc25bb267d7af55404fb6d
Beicker Zambrano <beicker998@gmail.com>
2022-12-25 20:15:46 -0500

    Add explosion scene as child when hit

--- Asteroid.gd
+++ Asteroid.gd
@@ -4,6 +4,7 @@ extends RigidBody2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
+export(PackedScene) var explosion_scene
 
 
 # Called when the node enters the scene tree for the first time.
@@ -21,9 +22,24 @@ func _on_VisibilityNotifier2D_screen_exited():
 
 
 func _on_Area2D_area_entered(area):
-	print("Hit")
 	var groups: Array = area.get_groups()
 	if 	groups.has("Projectile"):
+		var explosion = explosion_scene.instance()
+		explosion.position = position
+		explosion.one_shot = true
+		explosion.emitting = true
+		get_tree().current_scene.add_child(explosion)
 		queue_free()
 
+	pass # Replace with function body.
+	
+func _on_Asteroid_body_entered(body):
+	var groups: Array = body.get_groups()
+	if 	groups.has("Asteroid"):
+		var explosion = explosion_scene.instance()
+		explosion.position = position
+		explosion.one_shot = true
+		explosion.emitting = true
+		get_tree().current_scene.add_child(explosion)
+		queue_free()
 	pass # Replace with function body.
